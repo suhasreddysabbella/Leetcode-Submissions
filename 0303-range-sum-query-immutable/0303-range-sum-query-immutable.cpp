edcb85ffb70900a7711c22cecddef7dacb164106
suhasreddysabbella <suhasreddysabbella@gmail.com>
2026-05-11 20:46:57 +0530

    Time: 0 ms (100%), Space: 23.9 MB (61.3%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,67 +1,37 @@
 class NumArray {
 public:
-    vector<int> segtree;
+    vector<int>bit;
+    vector<int>nums;
     int n;
-
-    NumArray(vector<int>& nums) {
-        n = nums.size();
-
-        segtree.resize(4 * n);
-
-        build(0, 0, n - 1, nums);
-    }
-
-    void build(int idx, int low, int high, vector<int>& nums) {
-
-        // Leaf node
-        if(low == high) {
-            segtree[idx] = nums[low];
-            return;
+    NumArray(vector<int>& arr) {
+        nums=arr;
+        n=arr.size();
+        bit.resize(n+1,0);
+        for(int i=0;i<n;i++){
+            add(i+1,arr[i]);
         }
-
-        int mid = (low + high) / 2;
-
-        build(2 * idx + 1, low, mid, nums);
-        build(2 * idx + 2, mid + 1, high, nums);
-
-        segtree[idx] =
-            segtree[2 * idx + 1] +
-            segtree[2 * idx + 2];
     }
-
-    int query(int idx, int low, int high,
-              int l, int r) {
-
-        // No overlap
-        if(high < l || low > r)
-            return 0;
-
-        // Complete overlap
-        if(low >= l && high <= r)
-            return segtree[idx];
-
-        // Partial overlap
-        int mid = (low + high) / 2;
-
-        int left =
-            query(2 * idx + 1,
-                  low,
-                  mid,
-                  l,
-                  r);
-
-        int right =
-            query(2 * idx + 2,
-                  mid + 1,
-                  high,
-                  l,
-                  r);
-
-        return left + right;
+    void add(int idx,int val){
+        while(idx<=n){
+            bit[idx]+=val;
+            idx+=(idx &-idx);
+        }
     }
-
+    int prefixsum(int idx){
+        int sum=0;
+        while(idx>0){
+            sum+=bit[idx];
+            idx-=(idx & -idx);
+        }
+        return sum;
+    }
     int sumRange(int left, int right) {
-
-        return query(0, 0, n - 1, left, right);
+        return prefixsum(right+1) - prefixsum(left);   
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
